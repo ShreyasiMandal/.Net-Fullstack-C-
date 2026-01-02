40214ab4ea1c20dd415301168fcc11563535a0dd
ShreyasiMandal <121492239+ShreyasiMandal@users.noreply.github.com>
2026-01-02 15:58:38 +0530

    Add files via upload

---
+++ Day6/HasinaCabs/cab.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace HasinaCabs;
+
+public class cab
+{
+    #region properties
+    public string CabType { get; set; }
+    public string BookingID { get; set; }
+    public int WaitingTime { get; set; }
+    public double Distance{ get; set; }
+
+    #endregion
+
+}
