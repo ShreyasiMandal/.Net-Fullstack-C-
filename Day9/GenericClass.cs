e7b9c8993400bd801c0e957348baac22284bd0dc
ShreyasiMandal <121492239+ShreyasiMandal@users.noreply.github.com>
2026-01-19 07:55:16 +0530

    Add files via upload

---
+++ Day9/GenericClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LPU_Common
+    
+    //Custom Generic Class created for Demo
+    
+{
+     public class GenericClass<T> //T denotes It is a Generic class
+    {
+
+        /// <summary>
+        /// Generic Method
+        /// </summary>
+        /// <param name="obj1"></param>
+        /// <param name="obj2"></param>
+     public void SwapMe(ref T obj1,ref T obj2)
+        {
+            T temp;
+            temp = obj1;
+            obj1 = obj2;
+            obj2 = temp;
+        }
+    }
+}
---
+++ Day9/StudentBL.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using LPU_Common;
+using System.Collections.Generic;
+using LPU_Entity;
+using LPU_DAL;
+using LPU_Exception;
+namespace LPU_BL
+{
+    public class StudentBL : IStudentCRUD
+    {
+        //if we make the object direct here 
+        //Student s1 = new Student();
+        //It is not a good practise cause the object is created before creating the constructor
+        //Like it is invoked before writing the constructor
+
+        StudentDAO sDao = null;  //declare the object and give null value 
+        //And initialize it in the constructor
+        //this is the right way
+        //The object is created at class level because theis can be used by the class members
+        //we need DAO (student_DAL) class objects
+        //IN the DAL classes all methods are instances method
+        public StudentBL()
+        {
+           sDao = new StudentDAO();
+
+        }
+        public bool DropStudentDetails(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool EnrollStudent(Student sObj)
+        {
+            return sDao.EnrollStudent(sObj);
+        }
+
+        public Student SearchStudentByID(int rollNo)
+        {
+            Student s1 = null;
+            try
+            {
+                s1 = sDao.SearchStudentByID(rollNo);
+            }catch(LPUException e)
+            {
+                throw e;
+                //Exception from DAL Layer
+                //Catch in BL 
+                //Throw in UI Layer
+            }
+            return s1;
+        }
+
+        public List<Student> SearchStudentByName(string name)
+        {
+            return sDao.SearchStudentByName(name);
+        }
+
+        public bool UpdateStudentDetails(int id, Student newObj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
