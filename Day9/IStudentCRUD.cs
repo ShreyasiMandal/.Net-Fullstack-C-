c2e8ca26f9fefe2b1d5426802f6f6c485a2cd102
ShreyasiMandal <121492239+ShreyasiMandal@users.noreply.github.com>
2026-01-19 07:51:00 +0530

    Add files via upload

---
+++ Day9/IStudentCRUD.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using LPU_Entity;
+using System.Collections.Generic;
+namespace LPU_Common
+{
+    public interface IStudentCRUD
+    {
+
+        //In interface every method is Abstract
+        Student SearchStudentByID(int rollNo);
+        List<Student> SearchStudentByName(string name);
+
+        bool EnrollStudent(Student sObj);
+        bool UpdateStudentDetails(int id, Student newObj);
+        bool DropStudentDetails(int id);
+    }
+}
